e32bf32f2f1f836b128db83b214f1493a4436a7a
David E. Beltran H <dbeltran486@gmail.com>
2016-05-06 23:21:15 -0430

    subida del proyecto y depenmdencias

---
+++ login/resources/views/login/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Login</title>
+        <link rel="stylesheet" href="/librerias/css/bootstrap.css">
+        <link rel="stylesheet" href="http://www.atlasestateagents.co.uk/css/tether.min.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-dark navbar-fixed-top bg-inverse">
+            <a class="navbar-brand" href="#">Login Lumen</a>
+        </nav>
+    </body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://www.atlasestateagents.co.uk/javascript/tether.min.js"></script>
+    <script src="/librerias/js/bootstrap.js"></script>
+</html>
